0c6de7ec90ec0eb974c5ccfc6ad348ce4f5b40d5
Suah Kim <skim460@aucklanduni.ac.nz>
2026-01-10 07:00:12 +1300

    feat: configure Docker containers and nginx for Cloud Run deployment

--- rxconfig.py
+++ rxconfig.py
@@ -1,9 +1,14 @@
+import os
+
 import reflex as rx
+
 
 config = rx.Config(
     app_name="suahkim_dev",
+    api_url=os.getenv("API_URL", "http://localhost:8080"),
+    backend_port=int(os.getenv("BACKEND_PORT", "8080")),
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
-    ]
+    ],
 )
